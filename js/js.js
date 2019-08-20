af346713e34f3bf95c338994664f098ee3332b33
Student <example@example.com>
2019-08-20 14:38:16 -0700

    almost working js

--- js/js.js
+++ js/js.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
+  console.log('Works!');
 
-$("form#identifiers").submit(function(event){
-  event.preventDefault();
-      $("input:checkbox[name=personality]:checked").each(function(){
-        var personalityTrait = $(this).val();
-        $('.responses')append.(personalityTrait);
+  $("form#identify").submit(function(event){
+    event.preventDefault();
+        $("input:checkbox[name=personality]:checked").each(function(){
+          var personalityTrait = parseInt((this).val());
+          var theTotal =[];
+          theTotal = theTotal.push(personalityTrait);
+          if (theTotal).length < 3{
+            $('responses').append("You Lose");
+          }
+          if (theTotal).length =3{
+            $("responses").append("Okie Doke");
+          }
+          console.log('Works!');
+    });
+
   });
-
-});
 });
